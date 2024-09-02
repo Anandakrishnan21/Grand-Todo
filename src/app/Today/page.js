53b3d4eb27c8853ad374e52befa6ced8b41aec01
Anandakrishnan21 <anandakrishnandinej02@gmail.com>
2024-09-02 23:59:14 +0530

    [added]: pagination

--- src/app/Today/page.js
+++ src/app/Today/page.js
@@ -1,5 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { RiDraggable } from "react-icons/ri";
+import { LuAlarmClock } from "react-icons/lu";
 
 function Today() {
   const [todo, setTodo] = useState([]);
@@ -27,15 +29,36 @@ function Today() {
     (item) => item.date == today.replace(/\//g, "-")
   );
 
+  const colorChange = (priority) => {
+    if (priority == "high") {
+      return "red-500";
+    } else if (priority == "medium") {
+      return "yellow-500";
+    } else {
+      return "green-500";
+    }
+  };
+
   return (
     <div className="flex justify-center items-center pt-4">
       <div className="w-[60%] h-[60%]">
         <h1 className="font-bold text-xl">Today</h1>
-        <span className="text-gray-600">{todayTodos.length} tasks</span>
+        <span className="text-gray-600 pb-2">{todayTodos.length} tasks</span>
         {todayTodos.map((item, index) => (
-          <div key={index} className="flex items-center gap-2 border-b p-2">
-            <div className="w-4 h-4 rounded-full border-[1px] border-gray-600"></div>
-            <p>{item.description}</p>
+          <div key={index} className="flex justify-between border-b p-2">
+            <div className="flex gap-2">
+              <RiDraggable size={20} />
+              <div className="w-4 h-4 rounded-full border-[1px] border-gray-600"></div>
+              <div className="flex flex-col">
+                <p>{item.description}</p>
+                <span className="text-sm text-gray-600">#inbox</span>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <p className={`text-${colorChange(item.priority)}`}>{item.priority}</p>
+              <p>2:00:00 Pm</p>
+              <LuAlarmClock size={20} />
+            </div>
           </div>
         ))}
       </div>
--- src/app/inbox/page.js
+++ src/app/inbox/page.js
@@ -1,12 +1,14 @@
 "use client";
 import { Checkbox, Pagination } from "antd";
 import React, { useEffect, useState } from "react";
+import { LuAlarmClock } from "react-icons/lu";
+import { RiDraggable } from "react-icons/ri";
 
 function InboxPage() {
   const [inbox, setInbox] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [paginationInbox, setPaginationInbox] = useState([]);
-  const itemsPerPage = 15;
+  const itemsPerPage = 8;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,32 +30,49 @@ function InboxPage() {
     setPaginationInbox(inbox.slice(startIndex, endIndex));
   }, [inbox, currentPage]);
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
+  const handleButtonLeft = () => {
+    if(currentPage > 1){
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  const handleButtonRight = () => {
+    if(currentPage < Math.ceil(inbox.length / itemsPerPage)){
+      setCurrentPage(currentPage + 1);
+    }
+  }
 
   return (
-    <div className="flex flex-col items-center text-sm font-medium leading-none gap-2 p-2">
-      <h2 className="w-[80%] items-start font-semibold text-lg">Inbox</h2>
-      {paginationInbox.map((data, index) => (
-        <div
-          key={index}
-          className="w-[80%] flex gap-2 items-center bg-stone-100 p-1 rounded-md"
-        >
-          <div>
-            <Checkbox/>
-          </div>
-          <p className="w-60">{data.description}</p>
-          <p>{data.assignee}</p>
-          <p>{data.priority}</p>
+    <div className="flex justify-center items-center pt-4">
+      <div className="w-[60%] h-[60%]">
+        <h1 className="font-bold text-xl">Inbox</h1>
+        <span className="text-gray-600 pb-2">
+          {inbox.length} tasks
+        </span>
+        <div className="flex justify-end gap-1">
+          <button onClick={handleButtonLeft} className="px-2 border-[1px] border-gray-500 rounded-md">L</button>
+          <button onClick={handleButtonRight} className="px-2 border-[1px] border-gray-500 rounded-md">R</button>
         </div>
-      ))}
-      {/* <Pagination
-        current={currentPage}
-        total={inbox.length}
-        pageSize={itemsPerPage}
-        onChange={handlePageChange}
-      /> */}
+        {paginationInbox.map((item, index) => (
+          <div key={index} className="flex justify-between border-b p-2">
+            <div className="flex gap-2">
+              <RiDraggable size={20} />
+              <div className="w-4 h-4 rounded-full border-[1px] border-gray-600"></div>
+              <div className="flex flex-col">
+                <p>{item.description}</p>
+                <span className="text-sm text-gray-600">#inbox</span>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <p>
+                {item.priority}
+              </p>
+              <p>2:00:00 Pm</p>
+              <LuAlarmClock size={20} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
